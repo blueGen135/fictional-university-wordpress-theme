5678d985103648030b3339c3035bdf1b86dcb540
Raaj <abap.5135@hotmail.com>
2022-08-15 22:25:50 +0530

    Custom post type and relationships,image support

--- functions.php
+++ functions.php
@@ -15,6 +15,89 @@ function university_features() {
   register_nav_menu( 'footerLocationOne', 'Footer Location One' );
   register_nav_menu( 'footerLocationTwo', 'Footer Location Two' );
   add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
+  add_image_size( 'professorLandscape', 400, 260 , true );
+  add_image_size( 'professorPortrait', 480, 650 , true );
 }
 
 add_action('after_setup_theme', 'university_features');
+
+/*Custom Post Type*/
+function university_post_type (){
+//Event Post Type
+  register_post_type( 'event', array(
+      'supports' => array('title', 'editor', 'excerpt'),
+      'rewrite' => array('slug' => 'events'),
+      'has_archive' => true,
+      'public' => true,
+      'show_in_rest' => true,
+      'labels' => array(
+        'name' => 'Events',
+        'add_new_item' => 'Add New Event',
+        'edit_item' => 'Edit Event',
+        'all_items' => 'All Events',
+        'singular_name' => 'Event'
+      ),
+      'menu_icon' => 'dashicons-calendar-alt'
+  ) );
+
+  //Program post type
+
+  register_post_type( 'program', array(
+      'supports' => array('title', 'editor'),
+      'rewrite' => array('slug' => 'programs'),
+      'has_archive' => true,
+      'public' => true,
+      'show_in_rest' => true,
+      'labels' => array(
+        'name' => 'Programs',
+        'add_new_item' => 'Add New Program',
+        'edit_item' => 'Edit Program',
+        'all_items' => 'All Programs',
+        'singular_name' => 'Program'
+      ),
+      'menu_icon' => 'dashicons-awards'
+  ) );
+
+  //Professor post type
+
+  register_post_type( 'professor', array(
+      'supports' => array('title', 'editor','thumbnail'),
+      'public' => true,
+      'show_in_rest' => true,
+      'labels' => array(
+        'name' => 'Professors',
+        'add_new_item' => 'Add New Professor',
+        'edit_item' => 'Edit Professor',
+        'all_items' => 'All Professors',
+        'singular_name' => 'Professor'
+      ),
+      'menu_icon' => 'dashicons-welcome-learn-more'
+  ) );
+}
+add_action( 'init', 'university_post_type' );
+
+//Custom query for events
+function university_adjust_queries($query){
+    $today = date('Ymd');
+  if (!is_admin() AND is_post_type_archive( 'program' ) AND $query->is_main_query()) {
+     $query->set('orderby', 'title');
+     $query->set('order', 'ASC');
+     $query->set('posts_per_page', -1);
+  }
+
+  if (!is_admin() AND is_post_type_archive( 'event' ) AND $query->is_main_query()) {
+      $query->set('meta_query', 'event_date');
+      $query->set('orderby', 'meta_value_num');
+      $query->set('order', 'ASC');
+      $query->set('meta_query', array(
+        array(
+          'key' => 'event_date',
+          'compare' => '>=',
+          'value' => $today,
+          'type' => 'numeric'
+        )
+      ));
+    }
+}
+add_action( 'pre_get_posts', 'university_adjust_queries');
